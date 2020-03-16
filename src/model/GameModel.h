76f66d2423f1b6eb94412c98d9a295eef5a1e533
Benjamin Baffy <BenjaminBaffy@gmail.com>
2020-03-16 20:48:53 +0100

    GameModel Maybe switched to Optional.

--- src/model/GameModel.h
+++ src/model/GameModel.h
@@ -1,6 +1,10 @@
 #ifndef WARP_GAMEMODEL_H
 #define WARP_GAMEMODEL_H
 
+#include <vector>
+#include <optional>
+#include <allegro5/allegro.h>
+
 #include "Field.h"
 #include "EntityType.h"
 
@@ -9,9 +13,6 @@
 #include "Stable/HqAttack.h"
 #include "Stable/HqDefense.h"
 #include "Stable/Special.h"
-
-#include <vector>
-#include <allegro5/allegro.h>
 
 class GameModel {
 private:
@@ -39,8 +40,11 @@ public:
     void buildTower(std::pair<int, int> position);
     bool isBuildable(EntityType type);
 
+    // Enemy interaction
+    void spawnEnemies();
+
     // Getters
-    neither::Either<std::out_of_range, Field> getField(std::pair<int, int> position);
+    std::optional<Field> getField(std::pair<int, int> position);
     int getGold() const { return this->gold; }
     int getPoints() const { return this->points; }
     int getWaveNumber() const { return this->waveNumber; }
