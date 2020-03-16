8aca55aad268ffe131a04cd986c4e3eba22f23a2
Benjamin Baffy <BenjaminBaffy@gmail.com>
2020-03-16 20:48:53 +0100

    GameModel Maybe switched to Optional.

--- src/model/Field.cpp
+++ src/model/Field.cpp
@@ -3,32 +3,32 @@
 Field::Field(std::pair<int, int> position){
     this->position = position;
     this->teamStatus = Team::Neutral;
-    this->tower = std::unique_ptr<Stable>();
+    this->tower = std::nullopt;
     this->movingEntities = std::vector<std::unique_ptr<Unstable>>();
 }
 
 void Field::buildTower(EntityType type) {
     switch (type) {
         case EntityType::typeFactory :
-            this->tower = std::unique_ptr<Stable>(new Factory(this->position));
+            this->tower = std::shared_ptr<Stable>(new Factory(this->position));
             return;
         case EntityType::typeStable_1:
-            this->tower = std::unique_ptr<Stable>(new Stable1(this->position));
+            this->tower = std::shared_ptr<Stable>(new Stable1(this->position));
             return;
         case EntityType::typeStable_2:
-            this->tower = std::unique_ptr<Stable>(new Stable1(this->position));
+            this->tower = std::shared_ptr<Stable>(new Stable1(this->position));
             return;
         case EntityType::typeStable_3:
-            this->tower = std::unique_ptr<Stable>(new Stable1(this->position));
+            this->tower = std::shared_ptr<Stable>(new Stable1(this->position));
             return;
         case EntityType::typeHqAttack:
-            this->tower = std::unique_ptr<Stable>(new HqAttack(this->position));
+            this->tower = std::shared_ptr<Stable>(new HqAttack(this->position));
             return;
         case EntityType::typeHqDefense:
-            this->tower = std::unique_ptr<Stable>(new HqDefense(this->position));
+            this->tower = std::shared_ptr<Stable>(new HqDefense(this->position));
             return;
         case EntityType::typeSpecial:
-            this->tower = std::unique_ptr<Stable>(new Special(this->position));
+            this->tower = std::shared_ptr<Stable>(new Special(this->position));
             return;
         default:
             return;
@@ -36,16 +36,21 @@ void Field::buildTower(EntityType type) {
 }
 
 void Field::updateEntities() {
-    this->tower->update();
+    if(this->tower.has_value()){
+        this->tower.value()->update();
+    }
     for(auto& me : this->movingEntities){
         me->update();
     }
 }
 
-std::shared_ptr<Stable> Field::getTower() const {
-    return this->tower;
+std::optional<std::shared_ptr<Stable>> Field::getTower() {
+    if(this->tower.has_value()){
+        return this->tower;
+    }
+    return std::nullopt;
 }
 
-std::vector<std::unique_ptr<Unstable>> Field::getMovingEntities() const {
+std::vector<std::unique_ptr<Unstable>> Field::getMovingEntities() {
     return this->movingEntities;
 }
