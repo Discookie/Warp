426387251bceca330e4f58d18ed2fd8b6ec4c93c
Boldizsár Poór <boldar99@gmail.com>
2020-04-19 19:29:06 +0200

    FieldEntityCallbackClass fix

--- src/model/FieldEntityCallbackClass.h
+++ src/model/FieldEntityCallbackClass.h
@@ -2,6 +2,7 @@
 #define WARP_FIELDENTITYCALLBACKCLASS_H
 
 #include <functional>
+
 class FieldEntity;
 
 class FieldEntityCallbackClass {
@@ -11,21 +12,20 @@ class FieldEntityCallbackClass {
     std::function<void(std::shared_ptr<FieldEntity> obj)> cb_die;
 
 public:
-    explicit FieldEntityCallbackClass(std::function<void(std::shared_ptr<FieldEntity> obj)> gm_produce,
-                                      std::function<void(std::shared_ptr<FieldEntity> obj)> gm_move,
-                                      std::function<void(std::shared_ptr<FieldEntity> obj)> gm_attack,
-                                      std::function<void(std::shared_ptr<FieldEntity> obj)> gm_die){
-        this->cb_produce = gm_produce;
-        this->cb_move = gm_move;
-        this->cb_attack = gm_attack;
-        this->cb_attack = gm_die;
-    }
+    FieldEntityCallbackClass(
+        std::function<void(std::shared_ptr<FieldEntity> obj)> gm_produce,
+        std::function<void(std::shared_ptr<FieldEntity> obj)> gm_move,
+        std::function<void(std::shared_ptr<FieldEntity> obj)> gm_attack,
+        std::function<void(std::shared_ptr<FieldEntity> obj)> gm_die)
+        : cb_produce(std::move(gm_produce)),
+          cb_move(std::move(gm_move)),
+          cb_attack(std::move(gm_attack)),
+          cb_die(std::move(gm_die)) {}
 
-    void produce(std::shared_ptr<FieldEntity> obj){this->cb_produce(obj);}
-    void move   (std::shared_ptr<FieldEntity> obj){this->cb_move(obj);}
-    void attack (std::shared_ptr<FieldEntity> obj){this->cb_attack(obj);}
-    void die    (std::shared_ptr<FieldEntity> obj){this->cb_die(obj);}
+    void produce(std::shared_ptr<FieldEntity> obj) { this->cb_produce(obj); }
+    void move(std::shared_ptr<FieldEntity> obj) { this->cb_move(obj); }
+    void attack(std::shared_ptr<FieldEntity> obj) { this->cb_attack(obj); }
+    void die(std::shared_ptr<FieldEntity> obj) { this->cb_die(obj); }
 };
 
-
-#endif //WARP_FIELDENTITYCALLBACKCLASS_H
+#endif  // WARP_FIELDENTITYCALLBACKCLASS_H
