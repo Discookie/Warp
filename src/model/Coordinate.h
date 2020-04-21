78e51946db25a6e984560e575ec9b7bb2913c012
Benjamin Baffy <BenjaminBaffy@gmail.com>
2020-04-21 19:22:10 +0200

    Coordinates distance, SniperTower attack

--- src/model/Coordinate.h
+++ src/model/Coordinate.h
@@ -13,17 +13,20 @@ struct Coordinate {
         is >> coordinate.x >> coordinate.y;
         return is;
     }
-
     int x;
     int y;
 };
 
-inline bool operator==(const Coordinate& lhs, const Coordinate& rhs) {
+inline bool operator==(const Coordinate &lhs, const Coordinate &rhs) {
     return lhs.x == rhs.x && lhs.y == rhs.y;
 }
 
-inline bool operator!=(const Coordinate& lhs, const Coordinate& rhs) {
+inline bool operator!=(const Coordinate &lhs, const Coordinate &rhs) {
     return lhs.x != rhs.x || lhs.y != rhs.y;
+}
+
+inline int distance(const Coordinate &lhs, const Coordinate &rhs) {
+    return abs(lhs.x - rhs.x) + abs(lhs.y - rhs.y);
 }
 
 #endif  // WARP_COORDINATE_H
--- src/model/Stable/SniperTower.h
+++ src/model/Stable/SniperTower.h
@@ -31,7 +31,26 @@ public:
                Constants::SNIPERTOWER_BASE_DAMAGE : Constants::SNIPERTOWER_UPGRADE_DAMAGE;
     }
 
-    void attack_entities(std::vector<std::vector<Field>> &) override {}
+    void attack_entities(std::vector<std::vector<Field>> &fields) override {
+        int nearest_distance = 10000;
+        int current_distance;
+        Coordinate target_position{-1, -1};
+        for (auto &fv: fields) {
+            for (auto &f:fv) {
+                if (f.get_team_status() == Team::TeamEnemy) {
+                    current_distance = distance(this->position, f.get_position());
+                    if (current_distance < nearest_distance) {
+                        nearest_distance = current_distance;
+                        target_position = f.get_position();
+                    }
+                }
+            }
+        }
+        if (0 <= target_position.x && target_position.x < 12 &&
+            0 <= target_position.y && target_position.y < 10) {
+            fields[target_position.x][target_position.y].get_moving_entities()[0]->take_damage(this->damage());
+        }
+    }
 
     void do_actions() override {
         if (time_counter % attack_speed() == 0) {
