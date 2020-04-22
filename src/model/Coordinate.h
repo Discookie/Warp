e3daa2d5e7ba381c733784859a786e954d63268f
Benjamin Baffy <BenjaminBaffy@gmail.com>
2020-04-22 09:39:19 +0200

    coordinates commit fix

--- src/model/Coordinate.h
+++ src/model/Coordinate.h
@@ -22,14 +22,6 @@ struct Coordinate {
     int y;
 };
 
-inline bool operator==(const Coordinate &lhs, const Coordinate &rhs) {
-    return lhs.x == rhs.x && lhs.y == rhs.y;
-}
-
-inline bool operator!=(const Coordinate &lhs, const Coordinate &rhs) {
-    return lhs.x != rhs.x || lhs.y != rhs.y;
-}
-
 inline int distance(const Coordinate &lhs, const Coordinate &rhs) {
     return abs(lhs.x - rhs.x) + abs(lhs.y - rhs.y);
 }
