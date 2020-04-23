4eb413d3c3fc80cf0103ee87497a3d529ca44782
Boldizsár Poór <boldar99@gmail.com>
2020-04-23 12:46:10 +0200

    Diff added

--- src/model/Constants.h
+++ src/model/Constants.h
@@ -2,130 +2,131 @@
 #define WARP_CONSTANTS_H
 
 namespace Constants {
+/// Difficulty constants
+
 // Game
-    static const int STARTING_GOLD = 0;
-    static const int GOLD_GEN_AMOUNT = 50;
-    static const int GOLD_GEN_SPEED = 100;
-    static const int WAVE_COUNTDOWN_TIME = 10000;
-    static const int WAVE_SPAWN_TIME_START = 0;
-    static const int WAVE_SPAWN_TIME_END = -500;
-    static const int WAVE_OVER_TIME = -1000;
-    static const int SPAWN_ENEMIES_TIME = 10;
-    static const int FINAL_WAVE_NUMBER = 5;
+static const int STARTING_GOLD         = 0;
+static const int GOLD_GEN_AMOUNT       = 50;
+static const int GOLD_GEN_SPEED        = 100;
+static const int WAVE_COUNTDOWN_TIME   = 10000;
+static const int WAVE_SPAWN_TIME_START = 0;
+static const int WAVE_SPAWN_TIME_END   = -500;
+static const int WAVE_OVER_TIME        = -1000;
+static const int SPAWN_ENEMIES_TIME    = 10;
+static const int FINAL_WAVE_NUMBER     = 5; //
 
 /// Points
-    static const int POINTS_FOR_TOWER_BUILD = 10;
-    static const int POINTS_FOR_TOWER_UPGRADE = 5;
-    static const int POINTS_FOR_TOWER_REMOVE = 10;
+static const int POINTS_FOR_TOWER_BUILD   = 10;
+static const int POINTS_FOR_TOWER_UPGRADE = 5;
+static const int POINTS_FOR_TOWER_REMOVE  = 10;
 
-    // Stable
+// Stable
 /// Factory
-    static const int FACTORY_BASE_COST = 100;
-    static const int FACTORY_UPGRADE_COST = 100;
-    static const int FACTORY_MAX_HP = 1000;
-    static const int FACTORY_DAMAGE = 0;
-    static const int FACTORY_ATTACK_SPEED = 0;
-    static const int FACTORY_BASE_PRODUCTION = 10;
-    static const int FACTORY_UPGRADE_PRODUCTION = 20;
-    static const int FACTORY_BASE_PRODUCTION_SPEED = 10;
-    static const int FACTORY_UPGRADE_PRODUCTION_SPEED = 5;
-    static const int FACTORY_BASE_REMOVE_VALUE = 50;
-    static const int FACTORY_UPGRADE_REMOVE_VALUE = 100;
+static const int FACTORY_BASE_COST                = 100; //
+static const int FACTORY_UPGRADE_COST             = 100; //
+static const int FACTORY_MAX_HP                   = 1000;
+static const int FACTORY_DAMAGE                   = 0;
+static const int FACTORY_ATTACK_SPEED             = 0;
+static const int FACTORY_BASE_PRODUCTION          = 10;
+static const int FACTORY_UPGRADE_PRODUCTION       = 20;
+static const int FACTORY_BASE_PRODUCTION_SPEED    = 10;
+static const int FACTORY_UPGRADE_PRODUCTION_SPEED = 5;
+static const int FACTORY_BASE_REMOVE_VALUE        = 50;
+static const int FACTORY_UPGRADE_REMOVE_VALUE     = 100;
 
 /// LaserTower
-    static const int LASERTOWER_BASE_COST = 100;
-    static const int LASERTOWER_UPGRADE_COST = 100;
-    static const int LASERTOWER_MAX_HP = 1000;
-    static const int LASERTOWER_BASE_DAMAGE = 100;
-    static const int LASERTOWER_UPGRADE_DAMAGE = 100;
-    static const int LASERTOWER_BASE_ATTACKSPEED = 100;
-    static const int LASERTOWER_UPGRADE_ATTACKSPEED = 100;
-    static const int LASERTOWER_BASE_REMOVE_VALUE = 50;
-    static const int LASERTOWER_UPGRADE_REMOVE_VALUE = 100;
+static const int LASERTOWER_BASE_COST            = 100; //
+static const int LASERTOWER_UPGRADE_COST         = 100; //
+static const int LASERTOWER_MAX_HP               = 1000;
+static const int LASERTOWER_BASE_DAMAGE          = 100; //
+static const int LASERTOWER_UPGRADE_DAMAGE       = 100; //
+static const int LASERTOWER_BASE_ATTACKSPEED     = 100;
+static const int LASERTOWER_UPGRADE_ATTACKSPEED  = 100;
+static const int LASERTOWER_BASE_REMOVE_VALUE    = 50;
+static const int LASERTOWER_UPGRADE_REMOVE_VALUE = 100;
 
-
-    /// TeslaCoil
-    static const int TESLACOIL_BASE_COST = 100;
-    static const int TESLACOIL_UPGRADE_COST = 100;
-    static const int TESLACOIL_MAX_HP = 1000;
-    static const int TESLACOIL_BASE_DAMAGE  = 100;
-    static const int TESLACOIL_UPGRADE_DAMAGE  = 100;
-    static const int TESLACOIL_BASE_ATTACKSPEED = 100;
-    static const int TESLACOIL_ATTACK_RANGE = 1;
-    static const int TESLACOIL_UPGRADE_ATTACKSPEED = 100;
-    static const int TESLACOIL_BASE_REMOVE_VALUE          = 50;
-    static const int TESLACOIL_UPGRADE_REMOVE_VALUE       = 100;
+/// TeslaCoil
+static const int TESLACOIL_BASE_COST            = 100; //
+static const int TESLACOIL_UPGRADE_COST         = 100; //
+static const int TESLACOIL_MAX_HP               = 1000;
+static const int TESLACOIL_BASE_DAMAGE          = 100; //
+static const int TESLACOIL_UPGRADE_DAMAGE       = 100; //
+static const int TESLACOIL_BASE_ATTACKSPEED     = 100;
+static const int TESLACOIL_ATTACK_RANGE         = 1;
+static const int TESLACOIL_UPGRADE_ATTACKSPEED  = 100;
+static const int TESLACOIL_BASE_REMOVE_VALUE    = 50;
+static const int TESLACOIL_UPGRADE_REMOVE_VALUE = 100;
 
 /// SniperTower
-    static const int SNIPERTOWER_BASE_COST    = 100;
-    static const int SNIPERTOWER_UPGRADE_COST = 100;
-    static const int SNIPERTOWER_MAX_HP  = 1000;
-    static const int SNIPERTOWER_BASE_DAMAGE  = 100;
-    static const int SNIPERTOWER_UPGRADE_DAMAGE  = 100;
-    static const int SNIPERTOWER_BASE_ATTACKSPEED  = 100;
-    static const int SNIPERTOWER_UPGRADE_ATTACKSPEED  = 100;
-    static const int SNIPERTOWER_BASE_REMOVE_VALUE          = 50;
-    static const int SNIPERTOWER_UPGRADE_REMOVE_VALUE       = 100;
+static const int SNIPERTOWER_BASE_COST            = 100; //
+static const int SNIPERTOWER_UPGRADE_COST         = 100; //
+static const int SNIPERTOWER_MAX_HP               = 1000;
+static const int SNIPERTOWER_BASE_DAMAGE          = 100;
+static const int SNIPERTOWER_UPGRADE_DAMAGE       = 100; //
+static const int SNIPERTOWER_BASE_ATTACKSPEED     = 100; //
+static const int SNIPERTOWER_UPGRADE_ATTACKSPEED  = 100;
+static const int SNIPERTOWER_BASE_REMOVE_VALUE    = 50;
+static const int SNIPERTOWER_UPGRADE_REMOVE_VALUE = 100;
 
 /// HqAttack
-    static const int HQATTACK_BASE_COST    = 100;
-    static const int HQATTACK_UPGRADE_COST = 0;
-    static const int HQATTACK_MAX_HP  = 1000;
-    static const int HQATTACK_DAMAGE  = 100;
-    static const int HQATTACK_ATTACKSPEED  = 100;
-    static const int HQATTACK_REMOVE_VALUE  = 100;
+static const int HQATTACK_BASE_COST    = 100;
+static const int HQATTACK_UPGRADE_COST = 0;
+static const int HQATTACK_MAX_HP       = 1000;
+static const int HQATTACK_DAMAGE       = 100;
+static const int HQATTACK_ATTACKSPEED  = 100;
+static const int HQATTACK_REMOVE_VALUE = 100;
 
 /// HqDefense
-    static const int HQDEFENSE_BASE_COST    = 100;
-    static const int HQDEFENSE_UPGRADE_COST = 0;
-    static const int HQDEFENSE_MAX_HP  = 1000;
-    static const int HQDEFENSE_DAMAGE  = 0;
-    static const int HQDEFENSE_ATTACKSPEED  = 100;
-    static const int HQDEFENSE_REMOVE_VALUE = 100;
+static const int HQDEFENSE_BASE_COST    = 100;
+static const int HQDEFENSE_UPGRADE_COST = 0;
+static const int HQDEFENSE_MAX_HP       = 1000;
+static const int HQDEFENSE_DAMAGE       = 0;
+static const int HQDEFENSE_ATTACKSPEED  = 100;
+static const int HQDEFENSE_REMOVE_VALUE = 100;
 
 /// Special
-    static const int SPECIAL_BASE_COST    = 100;
-    static const int SPECIAL_UPGRADE_COST = 0;
-    static const int SPECIAL_MAX_HP  = 0;
-    static const int SPECIAL_DAMAGE  = 100;
-    static const int SPECIAL_ATTACKSPEED = 100;
-    static const int SPECIAL_ATTACK_RANGE = 5;
-    static const int SPECIAL_REMOVE_VALUE = 0;
+static const int SPECIAL_BASE_COST    = 100;
+static const int SPECIAL_UPGRADE_COST = 0;
+static const int SPECIAL_MAX_HP       = 0;
+static const int SPECIAL_DAMAGE       = 100;
+static const int SPECIAL_ATTACKSPEED  = 100;
+static const int SPECIAL_ATTACK_RANGE = 5;
+static const int SPECIAL_REMOVE_VALUE = 0;
 
 // Unstable
 /// Friendly
-    static const int FRIENDLY_COST = 100;
-    static const int FRIENDLY_MAX_HP = 1000;
-    static const int FRIENDLY_DAMAGE = 100;
-    static const int FRIENDLY_ATTACKSPEED = 100;
-    static const int FRIENDLY_MOVESPEED = 100;
+static const int FRIENDLY_COST        = 100;
+static const int FRIENDLY_MAX_HP      = 1000;
+static const int FRIENDLY_DAMAGE      = 100;
+static const int FRIENDLY_ATTACKSPEED = 100;
+static const int FRIENDLY_MOVESPEED   = 100;
 
 /// Enemy
-    static const int ENEMY_COST = 0;
+static const int ENEMY_COST = 0;
 
 /// Alien
-    static const int ALIEN_MAX_HP = 1000;
-    static const int ALIEN_DAMAGE = 100;
-    static const int ALIEN_ATTACKSPEED = 100;
-    static const int ALIEN_MOVESPEED = 100;
-    static const int ALIEN_SPWN_BASE_FREQUENCY = 200;
-    static const int ALIEN_SPWN_WAVE_FREQUENCY = 100;
+static const int ALIEN_MAX_HP              = 1000; //
+static const int ALIEN_DAMAGE              = 100;
+static const int ALIEN_ATTACKSPEED         = 100;
+static const int ALIEN_MOVESPEED           = 100; //
+static const int ALIEN_SPWN_BASE_FREQUENCY = 200;
+static const int ALIEN_SPWN_WAVE_FREQUENCY = 100;
 
 /// Octopus
-    static const int OCTOPUS_MAX_HP = 1000;
-    static const int OCTOPUS_DAMAGE = 100;
-    static const int OCTOPUS_ATTACKSPEED = 100;
-    static const int OCTOPUS_MOVESPEED = 100;
-    static const int OCTOPUS_SPWN_BASE_FREQUENCY = 600;
-    static const int OCTOPUS_SPWN_WAVE_FREQUENCY = 300;
+static const int OCTOPUS_MAX_HP              = 1000; //
+static const int OCTOPUS_DAMAGE              = 100;
+static const int OCTOPUS_ATTACKSPEED         = 100;
+static const int OCTOPUS_MOVESPEED           = 100; //
+static const int OCTOPUS_SPWN_BASE_FREQUENCY = 600;
+static const int OCTOPUS_SPWN_WAVE_FREQUENCY = 300;
 
 /// Robot
-    static const int ROBOT_MAX_HP = 1000;
-    static const int ROBOT_DAMAGE = 100;
-    static const int ROBOT_ATTACKSPEED = 100;
-    static const int ROBOT_MOVESPEED = 100;
-    static const int ROBOT_SPWN_BASE_FREQUENCY = 1000;
-    static const int ROBOT_SPWN_WAVE_FREQUENCY = 500;
+static const int ROBOT_MAX_HP              = 1000; //
+static const int ROBOT_DAMAGE              = 100;
+static const int ROBOT_ATTACKSPEED         = 100;
+static const int ROBOT_MOVESPEED           = 100; //
+static const int ROBOT_SPWN_BASE_FREQUENCY = 1000;
+static const int ROBOT_SPWN_WAVE_FREQUENCY = 500;
 
-}
-#endif //WARP_CONSTANTS_H
+};  // namespace Constants
+#endif  // WARP_CONSTANTS_H
