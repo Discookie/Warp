d03394514f7b138378d1d29331b96d5f64691374
Boldizsár Poór <boldar99@gmail.com>
2020-04-23 12:46:10 +0200

    Diff added

---
+++ src/model/Difficulty.h
@@ -0,0 +1,14 @@
+//
+// Created by Boldizsár Poór (boldar99@gmail.com) on 2020. 04. 23..
+//
+
+#ifndef WARP_DIFFICULTY_H
+#define WARP_DIFFICULTY_H
+
+enum Difficulty {
+    DiffEasy = 5,
+    DiffNormal = 6 ,
+    DiffHard = 7,
+};
+
+#endif  // WARP_DIFFICULTY_H
